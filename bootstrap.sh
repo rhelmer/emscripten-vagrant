3a41f69368167e417c5645df5f7394b3092852da
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2014-08-19 09:55:12 -0700

    Throwing more into the bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,15 @@
 # Commands and other details taken from
 # https://github.com/jsmess/jsmess/wiki/How-to-build-and-test-JSMESS-0.153
 
+# Configurable constants
+export MESS_TEST_PLATFORM=coleco
+export MESS_TEST_GAME_FILE=smurfs.rom
+export MESS_GAME_DIR=/vagrant/games
+export MESS_BIOS_DIR=/vagrant/bios
+
 # Preliminaries
 export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update
 sudo apt-get install -y git g++ nodejs qt4-default libsdl-dev libsdl-ttf2.0-dev libfontconfig1-dev libxinerama-dev
 
@@ -12,6 +19,14 @@ sudo apt-get install -y git g++ nodejs qt4-default libsdl-dev libsdl-ttf2.0-dev 
 git clone https://github.com/jsmess/jsmess.git
 cd jsmess
 git submodule update --init --recursive
+
+# Link games/ and bios/
+cd ..
+rm -rf games bios
+ln -s $MESS_GAME_DIR
+ln -s $MESS_BIOS_DIR
+
+# Clang & emscripten-fastcomp
 cd third_party
 git clone https://github.com/kripken/emscripten-fastcomp
 cd emscripten-fastcomp
@@ -24,5 +39,21 @@ cd ../..
 mkdir build
 cd build
 ../configure --enable-optimized --disable-assertions --enable-targets=host,js
-make -j 4
+make -j 4 install
 ../../emscripten/emcc
+
+# Helpers
+cd ../../../helpers
+sudo chown vagrant .
+./genhelpers.sh
+
+# If you want MAME, uncomment these lines
+#cd ../mamehelpers/
+#sudo chown vagrant .
+#./genhelpers.sh
+
+# Make a MESS!
+
+./startmake.sh $MESS_PLATFORM
+cd ../..
+make SYSTEM=$MESS_PLATFORM GAME=$MESS_GAME_FILE test
